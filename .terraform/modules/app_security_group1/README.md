f2678dcc9bee7041d448899e1444ebfa14b188ee
Barbie0827 <barbie.rodrigo27@gmail.com>
2024-10-02 12:59:47 -0400

    updated

---
+++ .terraform/modules/app_security_group1/README.md
@@ -0,0 +1,2 @@
+# terraform-aws-security-groups
+# remote module
--- modules/sg/sg.tf
+++ modules/sg/sg.tf
@@ -1,23 +1,22 @@
+# Bastion Security Group
 module "bastion_security_group" {
   source  = "app.terraform.io/0227springcloud/security-groups/aws"
   version = "1.0.0"
-  vpc_id  = aws_vpc.vpc.id
+
+  vpc_id = aws_vpc.vpc.id
   security_groups = {
-    bastion_sg = {
-      description = "Security group for bastion host"
+    "bastion_sg" : {
+      description   = "Security group for bastion host"
       ingress_rules = [
         {
-          description = "ingress rule for ssh"
-          priority    = 200
           from_port   = 22
           to_port     = 22
           protocol    = "tcp"
-          cidr_blocks = ["0.0.0.0/0"]
+          cidr_blocks = [var.my_ip]  # Adjust to your needs
         }
       ]
       egress_rules = [
         {
-          description = "egress rule"
           from_port   = 0
           to_port     = 0
           protocol    = "-1"
@@ -28,76 +27,34 @@ module "bastion_security_group" {
   }
 }
  
+# Web Security Group
 module "web_security_group" {
-  source  = source  = "app.terraform.io/0227springcloud/security-groups/aws"
+  source  = "app.terraform.io/0227springcloud/security-groups/aws"
   version = "1.0.0"
-  vpc_id  = aws_vpc.vpc.id
+  
+  vpc_id = module.vpc.id
+ 
   security_groups = {
-    alb_web_sg = {
-      description = "Security group for web load balancer"
+    "web_sg" = {
+      description   = "Security group for web"
       ingress_rules = [
         {
-          description = "ingress rule for http"
-          priority    = 202
           from_port   = 80
           to_port     = 80
           protocol    = "tcp"
           cidr_blocks = ["0.0.0.0/0"]
         },
         {
-          description = "ingress rule for http"
-          priority    = 204
           from_port   = 443
           to_port     = 443
           protocol    = "tcp"
           cidr_blocks = ["0.0.0.0/0"]
-        }
-      ]
-      egress_rules = [
-        {
-          description = "egress rule"
-          from_port   = 0
-          to_port     = 0
-          protocol    = "-1"
-          cidr_blocks = ["0.0.0.0/0"]
-        }
-      ]
-    }
-  }
-}
- 
-module "web_security_group1" {
-  source  = source  = "app.terraform.io/0227springcloud/security-groups/aws"
-  version = "1.0.0"
-  vpc_id  = aws_vpc.vpc.id
-  security_groups = {
-    web_ec2_sg = {
-      description = "Security group for web servers"
-      ingress_rules = [
-        {
-          description = "ingress rule for http"
-          priority    = 202
-          from_port   = 80
-          to_port     = 80
-          protocol    = "tcp"
-          security_groups = [module.web_security_group.security_group_id["alb_web_sg"]]
         },
         {
-          description = "my_ssh"
-          priority    = 200
-          from_port   = 22
-          to_port     = 22
-          protocol    = "tcp"
-          cidr_blocks = ["${aws_instance.bastion.private_ip}/32"]
+          from_port       = 22
+          to_port         = 22
+          protocol        = "tcp"
           security_groups = [module.bastion_security_group.security_group_id["bastion_sg"]]
-        },
-        {
-          description = "ingress rule for https"
-          priority    = 204
-          from_port   = 443
-          to_port     = 443
-          protocol    = "tcp"
-          cidr_blocks = ["0.0.0.0/0"]
         }
       ]
       egress_rules = [
@@ -113,59 +70,49 @@ module "web_security_group1" {
   }
 }
  
+ 
+# App Security Group
 module "app_security_group" {
   source  = "app.terraform.io/0227springcloud/security-groups/aws"
   version = "1.0.0"
-  vpc_id  = aws_vpc.vpc.id
-  security_groups = {
-    alb_app_sg = {
-      description = "Security group for app loadbalancer"
-      ingress_rules = [
-        {
-          description     = "ingress rule for http"
-          priority        = 202
-          from_port       = 80
-          to_port         = 80
-          protocol        = "tcp"
-          security_groups = [module.web_security_group1.security_group_id["web_ec2_sg"]]
-        }
-      ]
-      egress_rules = [
-        {
-          description = "egress rule"
-          from_port   = 0
-          to_port     = 0
-          protocol    = "-1"
-          cidr_blocks = ["0.0.0.0/0"]
-        }
-      ]
-    }
-  }
-}
+  
+  vpc_id = module.vpc.id
  
-module "app_security_group1" {
- source  = "app.terraform.io/0227springcloud/security-groups/aws"
-  version = "1.0.0"
-  vpc_id  = aws_vpc.vpc.id
   security_groups = {
-    app_ec2_sg = {
-      description = "Security group for app servers"
+    "app_sg" = {
+      description   = "Security group for app instances"
       ingress_rules = [
         {
-          description     = "ingress rule for http"
-          priority        = 202
-          from_port       = 80
-          to_port         = 80
+          from_port       = 8080
+          to_port         = 8080
           protocol        = "tcp"
-          security_groups = [module.app_security_group.security_group_id["alb_app_sg"]]
+          security_groups = [module.web_security_group.security_group_id["web_sg"]]
         },
         {
-          description = "my_ssh"
-          priority    = 200
-          from_port   = 22
-          to_port     = 22
-          protocol    = "tcp"
-          cidr_blocks = ["${aws_instance.bastion.private_ip}/32"]
+          from_port       = 22
+          to_port         = 22
+          protocol        = "tcp"
+          security_groups = [module.web_security_group.security_group_id["bastion_sg"]]
+        }
+      ]
+      egress_rules = [
+        {
+          description = "egress rule"
+          from_port   = 0
+          to_port     = 0
+          protocol    = "-1"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      ]
+    },
+    "app_elb_sg" = {
+      description   = "Security group for ALB"
+      ingress_rules = [
+        {
+          from_port       = 8080
+          to_port         = 8080
+          protocol        = "tcp"
+          security_groups = [module.web_security_group.security_group_id["web_sg"]]
         }
       ]
       egress_rules = [
@@ -181,21 +128,24 @@ module "app_security_group1" {
   }
 }
  
+ 
+ 
+# Database Security Group
 module "db_security_group" {
   source  = "app.terraform.io/0227springcloud/security-groups/aws"
   version = "1.0.0"
-  vpc_id  = aws_vpc.vpc.id
+  
+  vpc_id = module.vpc.id
   security_groups = {
-    db_sg = {
-      description = "Security group for Database"
+    "db_sg" = {
+      description   = "Security group for database"
       ingress_rules = [
         {
-          description     = "ingress rule for DB"
-          priority        = 500
-          from_port       = 3306
-          to_port         = 3306
-          protocol        = "tcp"
-          security_groups = [module.app_security_group1.security_group_id["app_ec2_sg"]]
+          
+          from_port   = 3306 #database mysql port
+          to_port     = 3306
+          protocol    = "tcp"
+          security_groups = ["module.app_security_group.security_group_id"["app_sg"]]  
         }
       ]
       egress_rules = [
@@ -210,4 +160,3 @@ module "db_security_group" {
     }
   }
 }
- 
